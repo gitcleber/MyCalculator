f864160db95113783d2aa88a086e71b063c2a782
gitcleber <cleberhill@gmail.com>
2021-11-15 13:04:15 -0300

    - added logic for error handling

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -16,15 +16,28 @@ namespace Calculator
             bool OKBool = true;
 
             Console.WriteLine("Welcome to My Calculator.");
+            do
+            {
+                Console.Write("\nEnter first number: ");
+                OKBool = double.TryParse(Console.ReadLine(), out firstDouble);
 
-            Console.Write("\nEnter first number: ");
-            OKBool = double.TryParse(Console.ReadLine(), out firstDouble);
-            Console.Write("\nEnter second number: ");
-            OKBool = double.TryParse(Console.ReadLine(), out secondDouble);
+                if (OKBool)
+                {
+                    do
+                    {
+                        Console.Write("\nEnter second number: ");
+                        OKBool = double.TryParse(Console.ReadLine(), out secondDouble);
 
-            totalDouble = firstDouble + secondDouble;
+                        if (OKBool)
+                        {
+                            totalDouble = firstDouble + secondDouble;
 
-            Console.WriteLine($"\n{firstDouble} + {secondDouble} = {totalDouble}\n");
+                            Console.WriteLine("\n{0} + {1} = {2}\n",
+                                firstDouble, secondDouble, totalDouble);
+                        }
+                    } while (!OKBool);
+                }
+            } while (!OKBool);
 
             Console.WriteLine("Press ENTER key to end program.");
             Console.ReadLine();
