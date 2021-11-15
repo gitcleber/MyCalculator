111138e0efa6f94807e69127788772731c5062a7
gitcleber <cleberhill@gmail.com>
2021-11-15 12:33:34 -0300

    - Create variables and constants for Calculator app.

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -4,8 +4,17 @@ namespace Calculator
 {
     class Program
     {
+
+        private const double _MIN_VALUE_Double = -1000;
+        private const double _MAX_VALUE_Double = 1000;
+
         static void Main(string[] args)
         {
+            double firstDouble = 0;
+            double secondDouble = 0;
+            double totalDouble = 0;
+            bool OKBool = true;
+
             Console.WriteLine("Welcome to My Calculator.");
             Console.Write("Press ENTER key to end program.");
             Console.ReadLine();
