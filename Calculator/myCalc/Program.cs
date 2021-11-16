80aee1a14e206b75629a846e965176b83094999c
gitcleber <cleberhill@gmail.com>
2021-11-15 21:38:35 -0300

    - Update myCalc app, version 1 - complete;

---
+++ Calculator/myCalc/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace myCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numCalc1, numCalc2;
+            string input;
+            bool OKBool;
+            char operation;
+
+            do {
+                Console.WriteLine("Calculadora - v1\n");
+                Console.WriteLine("Sair - 's'");
+                Console.Write(">>> ");
+                input = Console.ReadLine();
+                OKBool = Double.TryParse(input,out numCalc1);
+                if (OKBool)
+                {
+                    do
+                    {
+                        Console.WriteLine("soma: '+' subtração: '-' divisão: '/' multiplicação: '*' - Zerar: 'z'");
+                        Console.Write($"\n>>> {numCalc1} ");
+                        operation = Console.ReadKey().KeyChar;
+                        if ((operation == '+') || (operation == '-') || (operation == '/') || (operation == '*'))
+                        {
+                            do
+                            {
+                                Console.WriteLine("\nZerar: 'z'");
+                                Console.Write($"\n>>> {numCalc1} {operation} ");
+                                input = Console.ReadLine();
+                                OKBool = Double.TryParse(input, out numCalc2);
+
+                                if (OKBool)
+                                {
+                                    switch (operation)
+                                    {
+                                        case '+':
+                                            numCalc1 += numCalc2;
+                                            break; // break that closes the case
+
+                                        case '-':
+                                            numCalc1 -= numCalc2;// Some code here
+                                            break; // break that closes the case
+                                        case '/':
+                                            numCalc1 /= numCalc2;// Some code here
+                                            break; // break that closes the case
+
+                                        case '*':
+                                            numCalc1 *= numCalc2;// Some code here
+                                            break; // break that closes the case
+                                    }
+                                }
+                                else if (input == "z")
+                                    OKBool = true;
+
+                            } while (!OKBool);
+                        }
+                        else if (operation == 'z')
+                            OKBool = false;
+                    } while (OKBool);
+                }
+                else if (input == "s")
+                    OKBool = true;
+            } while (!OKBool);
+        }
+    }
+}
